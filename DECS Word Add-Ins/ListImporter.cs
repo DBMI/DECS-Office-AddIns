0153ead65c59d4578c322c4795702a2c81d87d31
kevin-dbmi <kjdelaney@ucsd.edu>
2023-05-04 16:06:35 -0700

    Adding ListImporter

---
+++ DECS Word Add-Ins/ListImporter.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.Office.Interop.Word;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.LinkLabel;
+
+namespace DecsWordAddIns
+{
+    internal class ListImporter
+    {
+        private const int MAX_LINES_PER_IMPORT = 1000;
+        private const string PREAMBLE = "INSERT INTO MRN_LIST (MRN)\r\nVALUES(\r\n";
+        private const string SUFFIX = ");";
+
+        public ListImporter() 
+        {
+        }
+
+        public void Scan(Document doc)
+        {
+            // Initialize the output .SQL file.
+            string output_filename = Utilities.FormOutputFilename(filename: doc.FullName, filetype: ".sql");
+
+            using (StreamWriter writer = new StreamWriter(output_filename))
+            {
+                writer.Write(PREAMBLE);
+                int num_lines = doc.Paragraphs.Count;
+                int lines_written = 0;
+                int lines_written_this_chunk = 0;
+
+                foreach (Paragraph para in doc.Paragraphs)
+                {
+                    if (para == null) continue;
+
+                    string line = para.Range.Text.ToString().Trim();
+
+                    if (line == null) continue;
+
+                    string line_ending;
+
+                    writer.Write("'" + line + "'");
+                    lines_written++;
+                    lines_written_this_chunk++;
+
+                    if (lines_written < num_lines)
+                    {
+                        if (lines_written_this_chunk < MAX_LINES_PER_IMPORT)
+                        {
+                            line_ending = ",\r\n";
+                        }
+                        else
+                        {
+                            line_ending = SUFFIX + "\r\n" + PREAMBLE;
+                            lines_written_this_chunk = 0;
+                        }
+                    }
+                    else
+                    {
+                        line_ending = SUFFIX + "\r\n";
+                    }
+                    
+                    writer.Write(line_ending);
+                }
+            }
+
+            string message = "Created file '" + output_filename + "'.";
+            MessageBox.Show(message, "Success");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,7 +1,9 @@
-# ICD Extractor ![image info](./ICDextractor/pictures/icd_to_sql_mini.png) 
+# DECS Word Add-Ins ![image info](./DECS%20Word%20Add-Ins/pictures/icd_to_sql_mini.png) 
 
-![Last Commit Date](./ICDextractor/.github/badges/last-commit-badge.svg?dummy=8484744)
+![Last Commit Date](./DECS%20Word%20Add-Ins/.github/badges/last-commit-badge.svg?dummy=8484744)
 
-Creates a custom button in Microsoft Word that allows user to scan a statement of work file & create SQL code that searches for the ICD-10 codes and names listed in the SoW.
+Creates a custom buttons in Microsoft Word that allows user to:
+* Scan a statement of work file & create SQL code that searches for the ICD-10 codes and names listed in the SoW.
+* Turn a list of MRNs into a SQL snippet that imports the list into a query.
 
-![image info](./ICDextractor/pictures/toolbar.png)
+![image info](./DECS%20Word%20Add-Ins/pictures/toolbar.png)
