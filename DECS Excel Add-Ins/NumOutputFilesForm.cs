d9a97ea4b66acead000ce749826c3c2975ee78e2
Kevin J. Delaney <kjdelaney@ucsd.edu>
2025-04-11 15:17:05 -0700

    Allow user to break up output into multiple files.

---
+++ DECS Excel Add-Ins/NumOutputFilesForm.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace DECS_Excel_Add_Ins
+{
+    public partial class NumOutputFilesForm : Form
+    {
+        public int? numFiles;
+
+        public NumOutputFilesForm()
+        {
+            InitializeComponent();
+        }
+
+        private void CancelButton_Click(object sender, EventArgs e)
+        {
+            numFiles = null;
+            this.DialogResult = DialogResult.Cancel;
+            this.Close();
+        }
+
+        private void OkButton_Click(object sender, EventArgs e)
+        {
+            numFiles = (int)Math.Floor(numFilesUpDown.Value);
+            this.DialogResult = DialogResult.OK;
+            this.Close();
+        }
+    }
+}
